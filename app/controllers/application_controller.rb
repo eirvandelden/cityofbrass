4c9b1553b7a10addc7cbb949aef9eb484612bacd
Etienne van Delden-de la Haije <eirvandelden@me.com>
2019-05-17 15:37:48 +0200

    Do not use exception when protecting from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include DetectFormatVariant
 
-  protect_from_forgery with: :exception
+  protect_from_forgery
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :clear_ajax_flash
