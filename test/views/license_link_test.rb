41c8411a468ed5d7c0fc8080d84c6c7a897fbeb6
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-05-28 20:27:54 +0200

    Remove misleading 4e GSL metadata

--- test/views/license_link_test.rb
+++ test/views/license_link_test.rb
@@ -13,4 +13,17 @@ class LicenseLinkTest < ActionView::TestCase
     assert_no_match(/rpg-system-attribution/, rendered)
     assert_no_match(/License d20 OGL/, rendered)
   end
+
+  test "does not render a modal link for 4th Edition" do
+    render partial: "layouts/license/link", locals: { core_rules: "4th Edition" }
+
+    assert_no_match(/scrolls\/license/, rendered)
+  end
+
+  test "does not render attribution for 4th Edition" do
+    render partial: "layouts/license/attribution", locals: { core_rules: "4th Edition" }
+
+    assert_no_match(/rpg-system-attribution/, rendered)
+    assert_no_match(/Game System License/, rendered)
+  end
 end
