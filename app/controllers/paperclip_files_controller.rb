745a1a9cfeadf12dbd0e176b8f2dc8dd76064075
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-06-03 14:19:53 +0200

    Secure paperclip file serving

--- app/controllers/paperclip_files_controller.rb
+++ app/controllers/paperclip_files_controller.rb
@@ -3,6 +3,9 @@ class PaperclipFilesController < ApplicationController
     file = storage_path.join(params[:path]).cleanpath
     return head :not_found unless file.to_s.start_with?("#{storage_path}/")
     return head :not_found unless file.file?
+    return head :not_found unless attachment_record
+    return head :not_found unless requested_attachment_path?(file)
+    return head :forbidden unless allowed_to_show_attachment?
 
     send_file file, disposition: "inline"
   end
@@ -10,6 +13,92 @@ class PaperclipFilesController < ApplicationController
   private
 
   def storage_path
-    Rails.root.join("storage", "paperclip", "gallery")
+    Rails.root.join("storage", "paperclip")
+  end
+
+  def attachment_record
+    @attachment_record ||= gallery_attachment_record || importer_attachment_record
+  end
+
+  def allowed_to_show_attachment?
+    return allowed_to_show_importer_attachment? if importer_attachment?
+
+    allowed_to_show_gallery_attachment?
+  end
+
+  def gallery_attachment_record
+    case path_segments
+    in [ "gallery", "residents", _, _, _, _, "images", id, _ ]
+      Gallery::ResidentImage.find_by(id: id)
+    in [ "gallery", "stock-images", id, _ ]
+      Gallery::Image.where(type: %w[Gallery::StockImage Gallery::ProprietaryImage]).find_by(id: id)
+    in [ "gallery", "map-images", id, _ ]
+      Gallery::MapImage.find_by(id: id)
+    in [ "gallery", "faq-images", id, _ ]
+      Gallery::FaqImage.find_by(id: id)
+    else
+      nil
+    end
+  end
+
+  def allowed_to_show_gallery_attachment?
+    case attachment_record
+    when Gallery::StockImage, Gallery::MapImage
+      true
+    when Gallery::ProprietaryImage, Gallery::FaqImage
+      admin_signed_in?
+    when Gallery::ResidentImage
+      admin_signed_in? || attachment_record.resident.user_id == current_user&.id
+    else
+      false
+    end
+  end
+
+  def requested_attachment_path?(file)
+    attachment_paths.include?(file.to_s)
+  end
+
+  def attachment_paths
+    attachment_styles.filter_map { |style| clean_attachment_path(style) }
+  end
+
+  def attachment_styles
+    attachment_record.file.styles.keys + [ :original ]
+  end
+
+  def clean_attachment_path(style)
+    Pathname.new(attachment_record.file.path(style)).cleanpath.to_s
+  end
+
+  def importer_attachment_record
+    case path_segments
+    in [ "importer", "previews", id, _ ]
+      Importer::PreviewFile.find_by(id: id)
+    in [ "importer", "imports", id, _ ]
+      Importer::ImportFile.find_by(id: id)
+    else
+      nil
+    end
+  end
+
+  def allowed_to_show_importer_attachment?
+    return true if admin_signed_in?
+    return false if importer_owner.blank?
+
+    importer_owner.user_id == current_user&.id
+  end
+
+  def importer_owner
+    return attachment_record.preview&.resident if attachment_record.is_a?(Importer::PreviewFile)
+
+    attachment_record.import&.resident
+  end
+
+  def importer_attachment?
+    attachment_record.is_a?(Importer::PreviewFile) || attachment_record.is_a?(Importer::ImportFile)
+  end
+
+  def path_segments
+    @path_segments ||= params[:path].to_s.split("/")
   end
 end
--- test/integration/paperclip_files_test.rb
+++ test/integration/paperclip_files_test.rb
@@ -1,15 +1,113 @@
 require "test_helper"
 
 class PaperclipFilesTest < ActionDispatch::IntegrationTest
-  test "serves gallery paperclip files from storage" do
+  include Devise::Test::IntegrationHelpers
+
+  test "does not serve unrecognized gallery files from storage" do
     file = Rails.root.join("storage", "paperclip", "gallery", "test.txt")
     FileUtils.mkdir_p(file.dirname)
     File.write(file, "paperclip")
 
     get "/paperclip/gallery/test.txt"
 
+    assert_response :not_found
+  ensure
+    FileUtils.rm_f(file) if file
+  end
+
+  test "does not serve resident image files to anonymous users" do
+    image = resident_image
+
+    get image.file.url(:original)
+
+    assert_response :forbidden
+  ensure
+    image&.destroy
+  end
+
+  test "serves resident image files to the owner" do
+    image = resident_image
+    sign_in users(:dan)
+
+    get image.file.url(:original)
+
     assert_response :success
-    assert_equal "paperclip", response.body
+  ensure
+    image&.destroy
+  end
+
+  test "serves public stock image files to anonymous users" do
+    image = stock_image
+
+    get image.file.url(:original)
+
+    assert_response :success
+  ensure
+    image&.destroy
+  end
+
+  test "does not serve arbitrary files in a public stock image directory" do
+    image = stock_image
+    path = "stock-images/#{image.id}/not_the_attachment.txt"
+    file = stored_gallery_file(path)
+    FileUtils.mkdir_p(file.dirname)
+    File.write(file, "stock")
+
+    get "/paperclip/gallery/#{path}"
+
+    assert_response :not_found
+  ensure
+    FileUtils.rm_f(file) if file
+    image&.destroy
+  end
+
+  test "does not serve proprietary image files to anonymous users" do
+    image = proprietary_image
+
+    get image.file.url(:original)
+
+    assert_response :forbidden
+  ensure
+    image&.destroy
+  end
+
+  test "serves importer preview files to the owner" do
+    preview = Importer::Preview.create!(resident: residents(:razune), mode: Importer::Preview::RESIDENT_CONTENT,
+                                        source: Importer::Preview::GAME_MASTER_5_XML, status: "parsing")
+    preview.add_uploads([ Rack::Test::UploadedFile.new(importer_fixture_file, "text/xml") ])
+    preview_file = preview.preview_files.first
+    sign_in users(:dan)
+
+    get preview_file.file.url
+
+    assert_response :success
+    assert_includes response.body, "<compendium"
+  end
+
+  test "does not serve importer preview files to anonymous users" do
+    preview = Importer::Preview.create!(resident: residents(:razune), mode: Importer::Preview::RESIDENT_CONTENT,
+                                        source: Importer::Preview::GAME_MASTER_5_XML, status: "parsing")
+    preview.add_uploads([ Rack::Test::UploadedFile.new(importer_fixture_file, "text/xml") ])
+
+    get preview.preview_files.first.file.url
+
+    assert_response :forbidden
+  end
+
+  test "does not serve arbitrary files in an importer preview file directory" do
+    preview = Importer::Preview.create!(resident: residents(:razune), mode: Importer::Preview::RESIDENT_CONTENT,
+                                        source: Importer::Preview::GAME_MASTER_5_XML, status: "parsing")
+    preview.add_uploads([ Rack::Test::UploadedFile.new(importer_fixture_file, "text/xml") ])
+    preview_file = preview.preview_files.first
+    path = "importer/previews/#{preview_file.id}/not_the_uploaded_file.xml"
+    file = Rails.root.join("storage", "paperclip", path)
+    FileUtils.mkdir_p(file.dirname)
+    File.write(file, "<compendium />")
+    sign_in users(:dan)
+
+    get "/paperclip/#{path}"
+
+    assert_response :not_found
   ensure
     FileUtils.rm_f(file) if file
   end
@@ -24,5 +122,31 @@ class PaperclipFilesTest < ActionDispatch::IntegrationTest
     assert_response :not_found
   ensure
     FileUtils.rm_f(file) if file
+  end
+
+  private
+
+  def stored_gallery_file(path)
+    Rails.root.join("storage", "paperclip", "gallery", path)
+  end
+
+  def resident_image
+    Gallery::ResidentImage.create!(name: "Resident", resident: residents(:razune), file: image_upload)
+  end
+
+  def stock_image
+    Gallery::StockImage.create!(name: "Stock", file: image_upload)
+  end
+
+  def proprietary_image
+    Gallery::ProprietaryImage.create!(name: "Proprietary", file: image_upload)
+  end
+
+  def image_upload
+    Rack::Test::UploadedFile.new(Gallery::Engine.root.join("app/assets/images/gallery/blank_image.png"), "image/png")
+  end
+
+  def importer_fixture_file
+    Importer::Engine.root.join("test/fixtures/files/importer/sample_compendium.xml")
   end
 end
