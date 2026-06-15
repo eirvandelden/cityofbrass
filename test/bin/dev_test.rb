d9df74712f619a0b4230fc22a3942f9a5341924d
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-06-15 13:22:56 +0200

    Use foreman for development process startup

--- test/bin/dev_test.rb
+++ test/bin/dev_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
-require "open3"
-require "tmpdir"
 
 class BinDevTest < ActiveSupport::TestCase
-  test "bin/dev starts sidekiq and rails with app redis defaults" do
+  test "bin/dev runs foreman with the development procfile" do
     script = Rails.root.join("bin/dev")
     contents = script.read
 
@@ -11,10 +9,9 @@ class BinDevTest < ActiveSupport::TestCase
     assert_predicate script, :executable?
     assert_includes contents, 'export REDIS_URL="${REDIS_URL:-redis://localhost:6379/1}"'
     assert_includes contents, 'export PORT="${PORT:-3000}"'
-    assert_includes contents, "bundle exec sidekiq -c 8 -q imports -q default -q mailers -q paperclip &"
-    assert_includes contents, 'trap \'status=$?; cleanup; exit "$status"\' EXIT'
-    assert_includes contents, 'bin/rails server "$@" &'
-    assert_includes contents, 'wait "$rails_pid"'
+    assert_includes contents, 'exec bundle exec foreman start -f Procfile.dev -p "$PORT" "$@"'
+    assert_not_includes contents, "bundle exec sidekiq"
+    assert_not_includes contents, "trap"
     assert_not_includes contents, "gem install"
   end
 
@@ -49,64 +46,5 @@ class BinDevTest < ActiveSupport::TestCase
     stack = ConnectionPool::TimedStack.new
 
     assert_raises(ConnectionPool::TimeoutError) { stack.pop(0) }
-  end
-
-  test "bin/dev stops sidekiq when rails server exits" do
-    sidekiq_pid = nil
-
-    Dir.mktmpdir do |dir|
-      install_bin_dev(dir)
-      install_bundle_stub(dir)
-      install_rails_stub(dir)
-
-      _, stderr, status = Open3.capture3(bin_dev_env(dir), "#{dir}/bin/dev", chdir: dir)
-      sidekiq_pid = File.read("#{dir}/sidekiq.pid").to_i
-
-      assert status.success?, stderr
-      assert_not process_alive?(sidekiq_pid), "expected sidekiq process #{sidekiq_pid} to stop"
-    ensure
-      Process.kill("TERM", sidekiq_pid) if sidekiq_pid && process_alive?(sidekiq_pid)
-    end
-  end
-
-  private
-
-  def install_bin_dev(dir)
-    FileUtils.mkdir_p("#{dir}/bin")
-    FileUtils.cp Rails.root.join("bin/dev"), "#{dir}/bin/dev"
-    FileUtils.chmod 0o755, "#{dir}/bin/dev"
-  end
-
-  def install_bundle_stub(dir)
-    File.write("#{dir}/bin/bundle", <<~SH)
-      #!/usr/bin/env sh
-      exec >/dev/null 2>/dev/null
-      echo "$$" > "$TEST_SIDEKIQ_PID_FILE"
-      trap 'exit 0' TERM INT
-      while true; do sleep 1; done
-    SH
-    FileUtils.chmod 0o755, "#{dir}/bin/bundle"
-  end
-
-  def install_rails_stub(dir)
-    File.write("#{dir}/bin/rails", <<~SH)
-      #!/usr/bin/env sh
-      exit 0
-    SH
-    FileUtils.chmod 0o755, "#{dir}/bin/rails"
-  end
-
-  def bin_dev_env(dir)
-    {
-      "PATH" => "#{dir}/bin:#{ENV.fetch("PATH")}",
-      "TEST_SIDEKIQ_PID_FILE" => "#{dir}/sidekiq.pid"
-    }
-  end
-
-  def process_alive?(pid)
-    Process.kill(0, pid)
-    true
-  rescue Errno::ESRCH
-    false
   end
 end
