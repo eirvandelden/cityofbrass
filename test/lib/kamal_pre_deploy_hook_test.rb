023b5369ad0972bf70ee30df7ff88cea8b26c59e
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-06-29 13:22:53 +0200

    Fix Kamal migration hook

---
+++ test/lib/kamal_pre_deploy_hook_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class KamalPreDeployHookTest < Minitest::Test
+  def test_runs_migrations_once_from_the_new_web_image
+    hook = Rails.root.join(".kamal/hooks/pre-deploy").read
+
+    assert_includes hook, 'kamal app exec --primary --roles web "bin/rails db:migrate"'
+    assert_equal false, hook.include?("--reuse")
+  end
+end
