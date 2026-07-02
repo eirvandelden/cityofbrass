d8a8a5a6db500c6c1112be78026007fe2b9e25be
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-02 10:36:51 +0200

    Remove dead paperclip patch and unused rails_12factor gem
    kt-paperclip 6.4.2 doesn't call URI.escape, so the Ruby-3.x
    URI::DEFAULT_PARSER compat patch (removed once already, then
    accidentally reintroduced by an unrelated XML-import commit) is dead
    code. rails_12factor is also unused now that production STDOUT
    logging is handled natively via RAILS_LOG_TO_STDOUT.
    
    Closes #48

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,24 +1,3 @@
-# Patch Paperclip 6.1.0 for Ruby 3.x compatibility: URI.escape was removed in Ruby 3.0.
-# Use URI::DEFAULT_PARSER.escape as the replacement.
-module Paperclip
-  class UrlGenerator
-    def escape_url(url)
-      if url.respond_to?(:escape)
-        url.escape
-      else
-        URI::DEFAULT_PARSER.escape(url).gsub(/[\?\(\)\[\]\+]/) { |m| "%#{m.ord.to_s(16).upcase}" }
-      end
-    end
-  end
-
-  class HttpUrlProxyAdapter < UriAdapter
-    def initialize(target, options = {})
-      escaped = URI::DEFAULT_PARSER.escape(target)
-      super(URI(target == URI::DEFAULT_PARSER.unescape(target) ? escaped : target), options)
-    end
-  end
-end
-
 Paperclip.options[:content_type_mappings][:xml] = %w[application/xml text/xml]
 
 module Paperclip
